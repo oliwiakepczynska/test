47d7c933d5480786d0ba0b0602441c9aa1d5a63f
DESKTOP-H793V9V\Oliwia <monchagrin@outlook.com>
2019-06-30 16:49:05 +0200

    commit

---
+++ src/test/java/oliwiakepczynska/whatsapp/service/SetupTest.java
@@ -0,0 +1,77 @@
+package com.whatsapp.service;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SetupTest {
+
+    static String localURL = "http://127.0.0.1:4723/wd/hub";
+    File androidApp = new File("src/resources/com.whatsapp.app.apk");
+    File iOSApp = new File("src/resources/appname.app");
+
+    public AndroidDriver getAndroidDriver() throws MalformedURLException {
+
+        AndroidDriver driver = null;
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName","Android Emulator");
+        caps.setCapability("platformName","ANDROID");
+        caps.setCapability("platformVersion", "5.0");
+        caps.setCapability("app", androidApp);
+
+        driver = new AndroidDriver(new URL(localURL),caps);
+
+        return driver;
+    }
+
+    public AndroidDriver getAndroidDriver(String URL) throws MalformedURLException {
+
+        AndroidDriver driver = null;
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName","Android Emulator");
+        caps.setCapability("platformName","ANDROID");
+        caps.setCapability("app", androidApp);
+
+        driver = new AndroidDriver(new URL(URL),caps);
+
+        return driver;
+    }
+
+    public IOSDriver getIOSDriver() throws MalformedURLException {
+
+        IOSDriver driver = null;
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName","iPhone 5");
+        caps.setCapability("platformName","IOS");
+        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("app", iOSApp);
+
+        driver = new IOSDriver(new URL(localURL),caps);
+
+        return driver;
+    }
+
+    public IOSDriver getIOSDriver(String URL) throws MalformedURLException {
+
+        IOSDriver driver = null;
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName","iPhone 5");
+        caps.setCapability("platformName","IOS");
+        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("app", iOSApp);
+
+        driver = new IOSDriver(new URL(URL),caps);
+
+        return driver;
+    }
+
+
+}
---
+++ src/test/java/oliwiakepczynska/whatsapp/testsuite/Register.java
@@ -0,0 +1,91 @@
+package com.whatsapp.testsuite;
+
+import com.whatsapp.service.SetupTest;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.util.List;
+
+import static com.whatsapp.resource_id.RegisterId.*;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class Register {
+
+    SetupTest s = new SetupTest();
+
+    AndroidDriver driver;
+
+    @BeforeTest
+    private void beforeTest() throws MalformedURLException {
+        driver = s.getAndroidDriver();
+    }
+
+    @AfterTest
+    private void afterTest() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkAlertPopup() {
+        try {
+            //Check alert is displayed
+            if (driver.findElementById(REG_TITLE_ALERT).isDisplayed()) {
+                //Click on OK button
+                clickOn(REG_BTN_OK);
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(dependsOnMethods = {"checkAlertPopup"})
+    public void clickOnAgree() {
+        clickOn(REG_BTN_AGREE_CONTINUE);
+    }
+
+    @Test(dependsOnMethods = {"clickOnAgree"}, dataProvider = "countryNameList")
+    public void selectCountry(String countryCode, String countryNameEnglish) {
+        clickOn(REG_BTN_SELECT_COUNTRY);
+
+        try {
+            driver.scrollTo("CountryName");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        sendKeys(REG_TEXT_SEARCH, countryCode);
+        List<WebElement> elements = driver.findElementsById(REG_TXT_COUNTRY_NAME_ENG);
+        for (WebElement element : elements) {
+            if (element.getText().equals(countryNameEnglish)) {
+                element.click();
+            } else {
+                assertTrue(false);
+            }
+        }
+    }
+
+    @DataProvider(name = "countryNameList")
+    public Object[][] listCountryName() {
+        return new Object[][]{
+                {"84", "Vietnam"},
+                {"379", "Vatican City"},
+                {"55", "Brazil"},
+                {"52", "Mexico"}
+        };
+    }
+
+    public void clickOn(String id) {
+        driver.findElement(By.id(id)).click();
+    }
+
+    public void sendKeys(String id, String keys) {
+        driver.findElement(By.id(id)).sendKeys(keys);
+    }
+
+}
